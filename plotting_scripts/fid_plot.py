fb46463df2fef702e8bbb35b16c5bd412dcc1308
Thang Tran <trankimthang279@gmail.com>
2024-02-27 11:06:16 +0100

    updated test results

--- plotting_scripts/fid_plot.py
+++ plotting_scripts/fid_plot.py
@@ -4,16 +4,16 @@ import numpy as np
 plt.rcParams.update({'font.size': 34})
 
 # names = ["Adder 10 qubits", "AQFT 6 qubits", "GHZ 24 qubits"]
-# i = [0.74,0.99, 0.99]
-# c = [0.77,0.95, 0.8]
+# i = [0.87, 0.96, 0.73]
+# c = [0.99, 0.95, 0.99]
 
-# names = ["HWE 10 qubits", "Supremacy 12 qubits", "Sycamore 12 qubits"]
-# i = [0.87, 0.94, 0.14]
-# c = [0.92, 0.76, 0.93]
+names = ["HWE 10 qubits", "Supremacy 12 qubits", "Sycamore 12 qubits"]
+i = [0.86, 0.12, 0.18]
+c = [0.98, 0.78, 0.96]
 
-names = ["Supremacy 20 qubits", "Supremacy 25 qubits"]
-i = [1.27E-05, 1.97E-31]
-c = [0.002, 1]
+# names = ["Supremacy 20 qubits", "Supremacy 25 qubits"]
+# i = [1.27E-05, 1.97E-31]
+# c = [0.002, 1]
 
 label1 = "original circuit"
 label2 = "cut circuit"
@@ -34,6 +34,6 @@ ax.set_xticks(x, names)
 ax.set_ylabel('Fidelity')
 ax.set_title(f"Compare fidelity between circuits")
 ax.legend(loc='upper left', ncols=3)
-ax.set_ylim(0, 1.1)
+ax.set_ylim(0, 1.2)
 
 plt.show()
